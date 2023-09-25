00f385d232557ebf6fbb39ba087af17135298707
Brian Gastón di Gangi Pedersen <briandigangi@hotmail.com>
2023-09-25 15:20:56 +0200

    +Index +Fixes Projects

---
+++ CurriculumVitae/Data/Portfolio.cs
@@ -0,0 +1,14 @@
+﻿namespace CurriculumVitae.Data
+{
+    public class Portfolio
+    {
+        public string? Title { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+
+    public class Skill
+    {
+        public string? Name { get; set; }
+        public string? Icon { get; set; }
+    }
+}
--- CurriculumVitae/Pages/Projects.cshtml.cs
+++ CurriculumVitae/Pages/Projects.cshtml.cs
@@ -23,32 +23,35 @@ namespace CurriculumVitae.Pages
                     },
                     new Projects
                     {
-                        Name = "Akvarie beregning Opgave",
-                        Description = "Et program som er lavet i console og beregner akvaries rumfang i kubikmeter ved givende input.",
+                        Name = "Aquarium volume calculator",
+                        Description = "The program is a console version and calculates the volume of an aquarium and shows the result in cm and liter.",
                         Url="https://github.com/Cabuxito/Akvarie-Opgave"
                     },
                     new Projects
                     {
                         Name = "Snake Game",
-                        Description = "Jeg har lavet et snake spil som er lavet på WPF.",
+                        Description = "I got a projekt where I did copy a Snake Game in WPF, add some features and do it more user responsive.",
                         Url= "https://github.com/Cabuxito/SnakeProjekt"
                     },
                     new Projects
                     {
-                        Name = "test",
+                        Name = "Intelligent IoT-Controlled House",
+                        Description = "I created a display output ( Screen ), where it shows a menu and some functions.",
+                        Url = "https://github.com/Cabuxito/EmbeddedOpgave"
 
                     },
                       new Projects
                     {
-                        Name = "Snake Game",
-                        Description = "Jeg har lavet et snake spil som er lavet på WPF.",
-                        Url= "https://github.com/Cabuxito/SnakeProjekt"
+                        Name = "eShop",
+                        Description = "I got a project about an eShop, I need to make database with EntityFrameworkCore and XunitTest all the methods," +
+                        " so we can implement razor pages next week and blazor after.",
+                        Url= "https://github.com/Cabuxito/eShopOpgave"
                     },
                         new Projects
                     {
-                        Name = "Snake Game",
-                        Description = "Jeg har lavet et snake spil som er lavet på WPF.",
-                        Url= "https://github.com/Cabuxito/SnakeProjekt"
+                        Name = "Blackjack Game",
+                        Description = "I have a projekt where I need to improve my skills with javaScript. So I need to do a BlackJack game on HTML and JS, and have some basics functions.",
+                        Url= "https://github.com/Cabuxito/HappyBody"
                     },
                 }
             );
